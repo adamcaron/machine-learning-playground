c0cc0cd5cc508a0ce0e071e67fa9815e947c00d1
Adam Caron <acaron@ip-10-21-14-11.us-west-2.compute.internal>
2017-11-17 10:38:20 -0700

    create dataframes

--- connect_to_psql.py
+++ connect_to_psql.py
@@ -25,21 +25,26 @@ def main():
 		traceback.print_exc()
 
 	try:
-		df = pd.read_sql_query("""SELECT * FROM common.video_views_fact LIMIT 100""", connection)
-		print df
+		df1 = pd.read_sql_query("""SELECT drupal_user_id, qualified_view FROM common.video_views_fact LIMIT 25""", connection)
+		df2 = pd.read_sql_query("""SELECT uid, tid FROM common.user_onboards LIMIT 25""", connection)
+		df3 = pd.read_sql_query("""SELECT drupal_user_id, onboarding_term, user_score FROM common.user_onboarding_fact WHERE user_score IS NOT NULL LIMIT 25""", connection)
+		print df1
+		print df2
+		print df3
 	except Exception, err:
 		print "Unable to read_sql_query.\n"
 		traceback.print_exc()
 
-	try:
-		cur.execute("""SELECT * FROM common.video_views_fact LIMIT 100""")
-		rows = cur.fetchall()
-		print "\nvideo_views_fact:\n"
-		for row in rows:
-			print "   ", row[0]
-	except Exception, err:
-		print "Cannot retreive.\n"
-		traceback.print_exc()
+	# try:
+	# 	cur.execute("""SELECT * FROM common.video_views_fact LIMIT 100""")
+	# 	rows = cur.fetchall()
+	# 	print "\nvideo_views_fact:\n"
+	# 	for row in rows:
+	# 		print "   ", row[0]
+	# except Exception, err:
+	# 	print "Cannot retreive.\n"
+	# 	traceback.print_exc()
+	connection.close()
 
 if __name__ == "__main__":
 	main()
